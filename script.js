8690941c1a1bdb6bd08a1b5bcd9d05ad94505014
dev_langesh <dev_langesh@dev.langesh>
2022-01-17 19:07:25 +0530

    Langesh commited

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+var r = 3.5;
+document.getElementById("range-result").innerHTML = r + "%";
+
+function compute() {
+  const p = document.getElementById("principal").value;
+  const n = document.getElementById("years").value;
+  const result = (p * n * r) / 100;
+
+  document.getElementById("result").innerHTML = `
+        <div>
+        If you deposit <span>${p}</span>
+        </div>
+        <div>
+        at an intrest rate of <span>${r}</span>
+        </div><div>
+        You will receive an amount of <span>${result}</span>.
+        </div><div>
+        in the year <span>${n}</span>         
+        </div>
+    `;
 }
-        
+
+function calcRange(val) {
+  r = val;
+  document.getElementById("range-result").innerHTML = r + "%";
+}
